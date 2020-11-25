7cdf6f1383fc0658a8320a17761519ce498da433
beatrizleticia <beatriz.olsen@etec.sp.gov.br>
2020-11-24 22:17:03 -0300

    att

--- src/app/bancodedados001.service.ts
+++ src/app/bancodedados001.service.ts
@@ -22,22 +22,92 @@ export class Bancodedados001Service {
       subtotal: 0
     },    {
       codigo: 3,
-      nome: 'Pepino',
-      imagem: 'assets/pepino.jpg',
+      nome: 'Manga',
+      imagem: 'assets/manga.jpg',
       quantidade: 0,
       preco: 5.49,
       subtotal: 0
     },    {
       codigo: 4,
-      produto: 'Cebola',
-      imagem: 'assets/cebola.jpg',
+      nome: 'Mamão',
+      imagem: 'assets/mamao.jpg',
       quantidade: 0,
       preco: 5.49,
       subtotal: 0
     },    {
       codigo: 5,
-      produto: 'Abacate',
+      nome: 'Abacate',
       imagem: 'assets/abacate.jpg',
+      quantidade: 0,
+      preco: 5.49,
+      subtotal: 0
+    },   {
+      codigo: 6,
+      nome: 'Banana',
+      imagem: 'assets/banana.jpg',
+      quantidade: 0,
+      preco: 5.49,
+      subtotal: 0
+    },   {
+      codigo: 7,
+      nome: 'Acerola',
+      imagem: 'assets/acerola.jpg',
+      quantidade: 0,
+      preco: 5.49,
+      subtotal: 0
+    },   {
+      codigo: 8,
+      nome: 'Ameixa',
+      imagem: 'assets/ameixa.jpg',
+      quantidade: 0,
+      preco: 5.49,
+      subtotal: 0
+    },   {
+      codigo: 9,
+      nome: 'Goiaba',
+      imagem: 'assets/goiaba.jpg',
+      quantidade: 0,
+      preco: 5.49,
+      subtotal: 0
+    },   {
+      codigo: 10,
+      nome: 'Kiwi',
+      imagem: 'assets/kiwi.jpg',
+      quantidade: 0,
+      preco: 5.49,
+      subtotal: 0
+    },   {
+      codigo: 11,
+      nome: 'Laranja',
+      imagem: 'assets/laranja.jpg',
+      quantidade: 0,
+      preco: 5.49,
+      subtotal: 0
+    },   {
+      codigo: 12,
+      nome: 'Limão',
+      imagem: 'assets/limao.jpg',
+      quantidade: 0,
+      preco: 5.49,
+      subtotal: 0
+    },   {
+      codigo: 13,
+      nome: 'Melão',
+      imagem: 'assets/melao.jpg',
+      quantidade: 0,
+      preco: 5.49,
+      subtotal: 0
+    },   {
+      codigo: 14,
+      nome: 'Pera',
+      imagem: 'assets/pera.jpg',
+      quantidade: 0,
+      preco: 5.49,
+      subtotal: 0
+    },   {
+      codigo: 15,
+      nome: 'Pessego',
+      imagem: 'assets/pessego.jpg',
       quantidade: 0,
       preco: 5.49,
       subtotal: 0
--- src/app/detalhes1/detalhes1.page.ts
+++ src/app/detalhes1/detalhes1.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+
+import { ActivatedRoute } from '@angular/router';
+import { Bancodedados001Service } from './../bancodedados001.service'
+
+
 @Component({
   selector: 'app-detalhes1',
   templateUrl: './detalhes1.page.html',
@@ -7,9 +12,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Detalhes1Page implements OnInit {
 
-  constructor() { }
+  produto = {
+    codigo: 0,
+    nome:'',
+    imagem:'',
+    quantidade: 0,
+    preco: 0,
+    subtotal: 0
+  };
+
+  constructor(
+    private recebendo: ActivatedRoute,
+    private bd: Bancodedados001Service
+  ) { }
 
   ngOnInit() {
+    this.recebendo.paramMap.subscribe(p =>{
+      let i: number = (Number(p.get('codigo')) - 1);
+      this.produto = {
+        codigo: this.bd.produtos[i].codigo,
+        nome: this.bd.produtos[i].nome,
+        imagem: this.bd.produtos[i].imagem,
+        quantidade: this.bd.produtos[i].quantidade,
+        preco: this.bd.produtos[i].preco,
+        subtotal: this.bd.produtos[i].subtotal,
+      }
+    });
   }
 
 }
