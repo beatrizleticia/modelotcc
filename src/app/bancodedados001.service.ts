4fb2efab5130bdb807034008bcfb5f41f4c1f273
beatrizleticia <beatriz.olsen@etec.sp.gov.br>
2020-11-30 21:27:23 -0300

    atualização
    30/11

--- src/app/bancodedados001.service.ts
+++ src/app/bancodedados001.service.ts
@@ -11,105 +11,105 @@ export class Bancodedados001Service {
       nome: 'Morango',
       imagem: 'assets/morango.jpg',
       quantidade: 0,
-      preco: 5.77,
+      preco: 6,
       subtotal: 0
     },    {
       codigo: 2,
       nome: 'Abacaxi',
       imagem: 'assets/abacaxi.jpg',
       quantidade: 0,
-      preco: 6.99,
+      preco: 7,
       subtotal: 0
     },    {
       codigo: 3,
       nome: 'Manga',
       imagem: 'assets/manga.jpg',
       quantidade: 0,
-      preco: 6.50,
+      preco: 6,
       subtotal: 0
     },    {
       codigo: 4,
       nome: 'Mamão',
       imagem: 'assets/mamao.jpg',
       quantidade: 0,
-      preco: 5.53,
+      preco: 5,
       subtotal: 0
     },    {
       codigo: 5,
       nome: 'Abacate',
       imagem: 'assets/abacate.jpg',
       quantidade: 0,
-      preco: 8.99,
+      preco: 9,
       subtotal: 0
     },   {
       codigo: 6,
       nome: 'Banana',
       imagem: 'assets/banana.jpg',
       quantidade: 0,
-      preco: 3.49,
+      preco: 4,
       subtotal: 0
     },   {
       codigo: 7,
       nome: 'Acerola',
       imagem: 'assets/acerola.jpg',
       quantidade: 0,
-      preco: 9.90,
+      preco: 10,
       subtotal: 0
     },   {
       codigo: 8,
       nome: 'Ameixa',
       imagem: 'assets/ameixa.jpg',
       quantidade: 0,
-      preco: 8.00,
+      preco: 8,
       subtotal: 0
     },   {
       codigo: 9,
       nome: 'Goiaba',
       imagem: 'assets/goiaba.jpg',
       quantidade: 0,
-      preco: 8.99,
+      preco: 9,
       subtotal: 0
     },   {
       codigo: 10,
       nome: 'Kiwi',
       imagem: 'assets/kiwi.jpg',
       quantidade: 0,
-      preco: 22.90,
+      preco: 23,
       subtotal: 0
     },   {
       codigo: 11,
       nome: 'Laranja',
       imagem: 'assets/laranja.jpg',
       quantidade: 0,
-      preco: 3.99,
+      preco: 4,
       subtotal: 0
     },   {
       codigo: 12,
       nome: 'Limão',
       imagem: 'assets/limao.jpg',
       quantidade: 0,
-      preco: 3.49,
+      preco: 4,
       subtotal: 0
     },   {
       codigo: 13,
       nome: 'Melão',
       imagem: 'assets/melao.jpg',
       quantidade: 0,
-      preco: 4.99,
+      preco: 5,
       subtotal: 0
     },   {
       codigo: 14,
       nome: 'Pera',
       imagem: 'assets/pera.jpg',
       quantidade: 0,
-      preco: 14.99,
+      preco: 15,
       subtotal: 0
     },   {
       codigo: 15,
       nome: 'Pessego',
       imagem: 'assets/pessego.jpg',
       quantidade: 0,
-      preco: 14.00,
+      preco: 14,
       subtotal: 0
     }
   ];
--- src/app/carrinhocompras/carrinhocompras.page.ts
+++ src/app/carrinhocompras/carrinhocompras.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Bancodedados001Service } from './../bancodedados001.service'
+import { Bancodedados001Service } from './../bancodedados001.service';
 
 @Component({
   selector: 'app-carrinhocompras',
@@ -10,38 +10,37 @@ import { Bancodedados001Service } from './../bancodedados001.service'
 export class CarrinhocomprasPage implements OnInit {
 
   total = 0;
-  teste = this.bd.produtos[0].quantidade;
   subtotal = 0;
 
 
   constructor(public bd:Bancodedados001Service) { }
-
+ 
   ngOnInit(){
 
   }
 
 
-  public maisItem(p: number){
+  public maisProd(p: number) {
     this.bd.produtos[p - 1].quantidade += 1;
     this.bd.produtos[p - 1].subtotal = this.bd.produtos[p - 1].quantidade * this.bd.produtos[p - 1].preco;
     this.calculaTotal();
   }
 
-  public menosItem(p: number){
-    this.bd.produtos[p - 1].quantidade += 1;
+  public menosProd(p: number) {
+    this.bd.produtos[p - 1].quantidade -= 1;
     this.bd.produtos[p - 1].subtotal = this.bd.produtos[p - 1].quantidade * this.bd.produtos[p - 1].preco;
     this.calculaTotal();
   }
 
-  calculaTotal(){
+  calculaTotal() {
     let t: number = 0;
     for(let i = 0; i < this.bd.produtos.length; i++){
-      t - t + this.bd.produtos[i].subtotal;
+      t = t + this.bd.produtos[i].subtotal;
     }
     return this.total = t;
   }
 
-  deletar(p: number){
+  deletar(p: number) {
     this.bd.excluir(p);
     this.calculaTotal();
   }
